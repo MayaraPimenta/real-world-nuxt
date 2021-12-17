bc88e8b4672f55372628bb4f0cba74e748270d63
Mayara Pimenta <mayara@primelan.com.br>
2021-12-17 14:20:14 -0300

    till class 07

---
+++ layouts/error.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <img src="../assets/img/ilustra_buscavazia.svg" alt="Fone desplugado">
+        <div class="title">
+            {{ message }}
+        </div>
+        <p v-if="statusCode === 404">
+            <nuxt-link to="/">
+              Que tal retornar para a página principal?
+            </nuxt-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            error: {
+                type: Object,
+                default: null
+            }
+        },
+        head() {
+            return {
+                title: this.message
+            }
+        },
+        computed: {
+            statusCode() {
+                return (this.error && this.error.statusCode) || 500
+            },
+            message() {
+                return this.error.message
+            }
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
